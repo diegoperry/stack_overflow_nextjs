c49fd92a056c636d22eac06b7a69cf0112b00f0a
diegoperry <diegoperry101@gmail.com>
2023-11-07 19:43:57 -0800

    layout fixes

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,4 @@
+import LocalSearch from "@/components/shared/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
@@ -5,13 +6,28 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <div>
-        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
-        <Link href="/ask-question">
-          <Button className="primary-gradient min-h-[46px] px-4 py-3">
+      <div className="flex w-full flex-col-reverse justify-between gap-4 md:flex-row md:items-center">
+        <h1 className="text-dark100_light900 sm:items-center lg:m-10 lg:ml-[250px]">
+          All Questions
+        </h1>
+        <Link
+          href="/ask-question"
+          className="flex w-full justify-end md:w-auto"
+        >
+          <Button className="primary-gradient !text-light-900 ml-[5%] min-h-[46px] px-4 py-3">
             Ask Question
           </Button>
         </Link>
+      </div>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center lg:ml-[200px]">
+        <LocalSearch
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
       </div>
     </>
   );
